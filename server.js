b18820f2a6ee4efca9fc9cc1b36eb62933a1f492
BaoTheMastr <smashsux@gmail.com>
2017-11-11 17:34:09 +0300

    express test

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-var http = require("http");
-function accept(req, res) {
-    res.writeHead(200, {
-        "Content-Type": "text/plain",
-        "Cache-Control": "no-cache"
-    });
+var express = require('express');
+var app = express();
 
-    res.end("ok");
-}
+app.set('port', (process.env.PORT || 5000));
 
-http.createServer(accept).listen(8080);
+// app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+//
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
